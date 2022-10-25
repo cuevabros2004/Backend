3f4cb7a8441bbf384625daa5e20f0e426b7fdf93
gcueva <cuevabros2004@hotmail.com>
2022-10-25 15:12:13 -0300

    Servidores Web - Servidor con Express

---
+++ main.js
@@ -0,0 +1,16 @@
+ 
+const { conectar } = require('./servidor')
+
+
+
+async function main(){
+    
+    try {
+        const serv = await conectar(8080)
+        console.log(`conectado al puerto ${serv.address().port}`)
+    } catch (error){
+        console.log('error' + error)
+    }
+}
+
+main()
---
+++ servidor.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const { Contenedor }   = require('./archivos.js') 
+
+const servidor = express()
+const contenedor = new Contenedor('productos.txt')
+
+function getAleatorio() {
+    return parseInt(Math.random() * 3) + 1
+}
+
+servidor.get('/productos',async (peticion, respuesta) =>{
+    respuesta.send( await contenedor.getAll())
+})
+
+//servidor.get('/despedida', (peticion, respuesta) =>{
+ //   respuesta.json({"codigo": "1", "nombreProducto": "SALÓN DE FIESTAS"})
+//})
+
+servidor.get('/productosRandom',async (peticion, respuesta) =>{
+    respuesta.send( await contenedor.getById(getAleatorio()))
+})
+
+
+
+function conectar (puerto = 0   ){
+    return new Promise((resolve, reject) =>{
+        const servidorConectador = servidor.listen(puerto, err =>{
+            if (err) 
+                reject(err)
+                //console.log("fallo" + err)
+            else
+                resolve(servidorConectador)
+                //console.log(`conectado al puerto ${servidorConectado.address().port}`)
+        })
+} )
+}
+
+module.exports = { conectar }
+
+
+ 
